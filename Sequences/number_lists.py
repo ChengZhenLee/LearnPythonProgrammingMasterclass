44621fc2e834ab95b82a1b2f3c90af5c789ee714
ChengZhenLee <zhensstories@gmail.com>
2024-03-10 18:00:49 +0100

    Completed Section 5: List and Tuples until video 113

--- Sequences/number_lists.py
+++ Sequences/number_lists.py
@@ -1,13 +1,43 @@
+empty_list = []
 even  = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-print(min(even))
-print(max(even))
-print(min(odd))
-print(max(odd))
+numbers = even + odd
+print(numbers)
 
-print(len(even))
-print(len(odd))
+sorted_numbers = sorted(numbers)    # creates a new list
+print(sorted_numbers)
+print(numbers)
 
-print()
-print("mississippi".count("issi"))
+digits = list("432985617")          # creates a new list
+print(digits)
+
+# more_numbers = list(numbers)      # creates a new list
+# more_numbers = numbers[:]         # creates a new list
+more_numbers = numbers.copy()       # creates a new list
+print(more_numbers)
+print(numbers is more_numbers)      # checks if they are the same list
+print(numbers == more_numbers)      # checks if the contents are the same
+
+# print(min(even))
+# print(max(even))
+# print(min(odd))
+# print(max(odd))
+
+# print(len(even))
+# print(len(odd))
+
+# print()
+# print("mississippi".count("issi"))
+
+# even.extend(odd)
+# print(even)
+# another_even = even
+# print(another_even)
+
+# even.sort()
+# print(even)
+
+# even.sort(reverse=True)
+# print(even)
+# print(another_even)
