ca2dfbf68b07a25951d6b115aaecd01ee9a97b09
ChengZhenLee <zhensstories@gmail.com>
2024-03-30 00:01:42 +0100

    Completed Section 9: Modules and Functions in Python until video 300

---
+++ modimport/turtledemo.py
@@ -0,0 +1,16 @@
+# import turtle
+from turtle import forward, right, done
+import turtle
+# import time
+
+# turtle.forward(150)
+# turtle.right(250)
+# turtle.forward(150)
+forward(150)
+right(250)
+turtle.circle(75)
+forward(150)
+
+# time.sleep(4)
+# turtle.done()
+done()
